c173cb01403e12f6406ab2df46efbf7cb036243d
haku1806 <50793831+haku1806@users.noreply.github.com>
2019-06-04 23:12:38 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,2 +1,5 @@
+sudo apt install tmux
+tmux
+
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.7.0/webchain-miner-2.7.0-linux-amd64.tar.gz && tar zxvf webchain-miner-2.7.0-linux-amd64.tar.gz && ./webchain-miner -o pool.webchain.network:3333 -u 0x8cc17f8e2840b9fc2afe80b587eb0f4306d7175a -p x
  
