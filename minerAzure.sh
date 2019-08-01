0195bcb82b9bff453316abfb65a2485eee437a51
haku1806 <50793831+haku1806@users.noreply.github.com>
2019-08-01 20:43:07 +0700

    Create minerAzure.sh

---
+++ minerAzure.sh
@@ -0,0 +1,5 @@
+wget https://github.com/webchain-network/webchain-miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz && tar zxvf webchain-miner-2.8.0-linux-amd64.tar.gz
+
+
+
+./webchain-miner -o pool.webchain.network:3333 -u 0x8cc17f8e2840b9fc2afe80b587eb0f4306d7175a -p x --max-cpu-usage 100 --donate-level 0 --worker-id Haku2
